5dc9b6eec89012e7f77e7295bda9f03874a5da56
Matteo-K <kervadecmatteo@gmail.com>
2025-03-12 14:42:43 +0100

    test fonction

--- html/test/test.js
+++ html/test/test.js
@@ -67,35 +67,43 @@ function moreTopLevelDomains() {
 
 const part1_test = [
   {
-    consigne : "Q1 - outsideTheContinent() : Tableau JS des pays (objets Country) dont au moins un pays frontalier n’est pas dans le même continent.",
+    enonce : "Q1 - outsideTheContinent()",
+    consigne : "Tableau JS des pays (objets Country) dont au moins un pays frontalier n’est pas dans le même continent.",
     fonction : outsideTheContinent
   },
   {
-    consigne : "Q2 - moreNeighbors() : Tableau  des  pays  ayant  le  plus  grand  nombre  de  voisins. Affichez aussi les voisins.",
+    enonce : "Q2 - moreNeighbors()",
+    consigne : "Tableau  des  pays  ayant  le  plus  grand  nombre  de  voisins. Affichez aussi les voisins.",
     fonction : moreNeighbors
   },
   {
-    consigne : "Q3 - neighborless() : Tableau des pays n’ayant aucun voisin.",
+    enonce : "Q3 - neighborless()",
+    consigne : "Tableau des pays n’ayant aucun voisin.",
     fonction : neighborless
   },
   {
-    consigne : "Q4 - moreLanguages() : Tableau des pays parlant le plus de langues. Affichez aussi les langues (objets Language).",
+    enonce : "Q4 - moreLanguages()",
+    consigne : "Tableau des pays parlant le plus de langues. Affichez aussi les langues (objets Language).",
     fonction : moreLanguages
   },
   {
-    consigne : "Q5 - withCommonLanguage() : Tableau des pays ayant au moins un voisin parlant l’une de  ses  langues.  Affichez  aussi  les  pays  voisins  (objets  Country)  et  les  langues  en question (objets Language).",
+    enonce : "Q5 - withCommonLanguage()",
+    consigne : "Tableau des pays ayant au moins un voisin parlant l’une de  ses  langues.  Affichez  aussi  les  pays  voisins  (objets  Country)  et  les  langues  en question (objets Language).",
     fonction : withCommonLanguage
   },
   {
-    consigne : "Q6 - withoutCommonCurrency() : Tableau  des  pays  sans  aucun  voisin ayant au moins une de ses monnaies",
+    enonce : "Q6 - withoutCommonCurrency()",
+    consigne : "Tableau  des  pays  sans  aucun  voisin ayant au moins une de ses monnaies",
     fonction : withoutCommonCurrency
   },
   {
-    consigne : "Q7 - sortingDecreasingDensity() : Tableau  des  pays  triés  par  ordre  décroissant  de densité de population.",
+    enonce : "Q7 - sortingDecreasingDensity()",
+    consigne : "Tableau  des  pays  triés  par  ordre  décroissant  de densité de population.",
     fonction : sortingDecreasingDensity
   },
   {
-    consigne : "Q8 - moreTopLevelDomains()  :  Tableau  des  pays  ayant  plusieurs Top Level Domains Internet.",
+    enonce : "Q8 - moreTopLevelDomains()",
+    consigne : "Tableau  des  pays  ayant  plusieurs Top Level Domains Internet.",
     fonction : moreTopLevelDomains
   }
 ]
@@ -106,8 +114,10 @@ function affiche_question(partie, questions) {
       <fieldset id="${partie}">
         <legend>Partie ${partie}</legend>
         ${questions.map(question => `
-          <label>${question.consigne}</label>
-          <button type="button" class="exec-btn" data-func="${question.fonction}">Exécuter</button>
+          <div>
+            <label><span>${question.enonce}&nbsp:</span>${question.consigne}</label>
+            <button type="button" class="exec-btn" data-func="${question.fonction}">Exécuter</button>
+          </div>
         `).join('')}
       </fieldset>
     </form>
