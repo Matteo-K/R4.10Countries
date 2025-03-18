46aa238961908f73a6c04a19a8c30d0386b792f8
BZHKylian <kylian.houedec.56@gmail.com>
2025-03-18 09:24:06 +0100

    je commiiiiiiiiiiiiiiiiiiiiiiit

--- html/v2/script_v2.js
+++ html/v2/script_v2.js
@@ -1,4 +1,7 @@
-function displayAllCountries(container_selecter, array) {
+const elementInPagination = 25;
+let pageActuelle = 1;
+
+function displayAllCountries(container_selector, array) {
   const html = `
     ${array.map(country => `
       <tr>
@@ -7,15 +10,56 @@ function displayAllCountries(container_selecter, array) {
         <td>${Math.round(country._area)} km<sup>2</sup></td>
         <td>${parseFloat(country.getPopDensity).toFixed(3)} hab/km<sup>2</sup></td>
         <td>${country._region}</td>
-        <td><img src="${country._img || "N/A"}" alt="Drapeau du pays ${country._name}" title="Saint-Vincent-et-les-Grenadines" ></td>
-        </td>
+        <td><img src="${country._img || "N/A"}" alt="Drapeau du pays ${country._name}" title="${country._name}" ></td>
       </tr>
     `).join('')}
   `;
 
-  $(container_selecter).append(html)
+  $(container_selector).append(html);
+}
+
+function paginations(array, nbElements = elementInPagination, progression) {
+  const totalPages = Math.ceil(array.length / nbElements);
+  removeAllElement("tbody");
+  pageActuelle += progression;
+
+  // Calcul de la tranche des pays à afficher pour la page actuelle
+  displayAllCountries("tbody", array.slice((pageActuelle-1) * nbElements, pageActuelle * nbElements));
+
+
+  // Mettre à jour les boutons de pagination
+  if (pageActuelle > 1 && pageActuelle < totalPages) {
+    $("#prec").prop("disabled", false);
+    $("#suiv").prop("disabled", false);
+  }
+  if (pageActuelle === 1) {
+    $("#prec").prop("disabled", true);
+  }
+  if (pageActuelle === totalPages) {
+    $("#suiv").prop("disabled", true);
+  }
+
+  // Mettre à jour l'affichage de la page
+  $(".spanPage").text(`Page ${pageActuelle}`);
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"});
+}
+
+function removeAllElement(container_selector) {
+  $(container_selector).empty();
 }
 
 $(document).ready(function () {
-  displayAllCountries("tbody", Object.values(Country.all_countries));
+  // Initialiser la pagination
+  paginations(Object.values(Country.all_countries), elementInPagination, 0);
+
+  // Actions sur les boutons de pagination
+  $("#suiv").click(function () {
+    paginations(Object.values(Country.all_countries), elementInPagination, 1);
+  });
+
+  $("#prec").click(function () {
+    paginations(Object.values(Country.all_countries), elementInPagination, -1);
+  });
 });
